c987aed442292033a9c8010560674c0671e063c1
AlexArtaud-Dev <smlartaudalexandre@gmail.com>
2024-10-31 11:13:15 +0100

    Fix

--- services/client.ts
+++ services/client.ts
@@ -2,12 +2,15 @@ import axios from "axios";
 
 import { getJwtToken } from "@/lib/utils";
 
+const inDevEnvironment = !!process && process.env.NODE_ENV === 'development';
+
 /**
  * This creates an Axios client instance with a base URL and default headers for content type.
  * It attaches an interceptor that adds the JWT token to the Authorization header of each request, if available.
  */
 const client = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BASE_API_URL,
+  // base URL should be the window.location.origin if in production mode, or the API URL if in development mode it should be the .env variable
+  baseURL: inDevEnvironment ? process.env.NEXT_PUBLIC_BASE_API_URL : "/v1",
   headers: {
     "Content-Type": "application/json",
   },
