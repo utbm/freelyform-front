62087be1ed66c918e8c485720c67f7b6dab6af65
AlexArtaud-Dev <smlartaudalexandre@gmail.com>
2024-11-08 11:05:57 +0100

    Lint + fix reverse proxy url

--- services/client.ts
+++ services/client.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 import { getJwtToken } from "@/lib/utils";
 
-const inDevEnvironment = !!process && process.env.NODE_ENV === "development";
+const behindReverseProxy = process.env.NEXT_PUBLIC_REVERSE_PROXY === "true";
 
 /**
  * This creates an Axios client instance with a base URL and default headers for content type.
@@ -10,7 +10,7 @@ const inDevEnvironment = !!process && process.env.NODE_ENV === "development";
  */
 const client = axios.create({
   // base URL should be the window.location.origin if in production mode, or the API URL if in development mode it should be the .env variable
-  baseURL: inDevEnvironment ? process.env.NEXT_PUBLIC_BASE_API_URL : "/v1",
+  baseURL: behindReverseProxy ? "/v1" : process.env.NEXT_PUBLIC_BASE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
